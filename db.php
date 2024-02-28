6f4e312d56d79d03fd0828a0d40c222527767151
sohammanjrekar <mrsohammanjrekar@gmail.com>
2024-02-28 21:16:04 +0530

    upload

--- db.php
+++ db.php
@@ -1,30 +1,25 @@
 <?php
-
 // Database connection parameters
-$hostname = 'localhost';  // Or your host IP
-$username = 'root';       // MySQL username
-$password = '1234';       // MySQL password
-$database = 'ecommerce';  // Database name
-
+$hostname = 'localhost';             // Hostname or IP address
+$port = 3306;                        // MySQL port
+$username = 'root';                  // MySQL username
+$password = '1234';                  // MySQL password
+$database = 'usersystem';            // Database name
 // Attempt to connect to MySQL database
-$connection = mysqli_connect($hostname, $username, $password, $database);
-
+$connection = mysqli_connect($hostname, $username, $password, $database, $port);
 // Check connection
 if (!$connection) {
     die("Database connection failed: " . mysqli_connect_error());
 } else {
     echo "Connected successfully";
 }
-
 // Example query execution
 $q = "SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO'";
-if ($connection->query($q) === TRUE) {
+if (mysqli_query($connection, $q)) {
     echo "SQL mode set successfully";
 } else {
-    echo "Error setting SQL mode: " . $connection->error;
+    echo "Error setting SQL mode: " . mysqli_error($connection);
 }
-
 // Close the connection
 mysqli_close($connection);
-
 ?>
